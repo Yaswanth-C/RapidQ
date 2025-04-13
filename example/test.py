a26dcdc37d51a7ab99e713867dd56ecc08924e62
Yaswanth C <yaswanthc4952@gmail.com>
2025-04-14 01:03:00 +0530

    added an example

---
+++ example/test.py
@@ -0,0 +1,11 @@
+from rapidq.decorators import background_task
+
+
+@background_task(name="simple-task")
+def test_func(msg):
+    print("simple task is running")
+    print(msg)
+
+
+if __name__ == "__main__":
+    test_func.in_background(msg="Hello, I'm running")
