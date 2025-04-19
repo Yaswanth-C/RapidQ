c0d444e515fc12a52cdb0a0a298467ba52ca45ae
Yaswanth C <yaswanthc4952@gmail.com>
2025-04-19 20:15:57 +0530

    task flushing separated

--- rapidq/startup.py
+++ rapidq/startup.py
@@ -21,7 +21,6 @@ def parse_args():
         "module",
         type=str,
         help="Module to import for the application to work.",
-        # required=True,
     )
     parser.add_argument(
         "-w",
@@ -30,26 +29,9 @@ def parse_args():
         default=CPU_COUNT,
         help="The number of worker processes to use.",
     )
-    parser.add_argument(
-        "--flush",
-        action="store_true",
-        help="Flush the broker and exit",
-    )
+
     args = parser.parse_args()
     return args
-
-
-def read_config_from_module(module_path: str):
-    module = import_module(module_path)
-
-    configurable_keys = (
-        "RAPIDQ_BROKER_SERIALIZER",
-        "RAPIDQ_BROKER_URL",
-    )
-    for key in configurable_keys:
-        if not getattr(module, key, None):
-            continue
-        os.environ[key] = str(getattr(module, key))
 
 
 def main():
@@ -57,16 +39,14 @@ def main():
     Main entry point for RapidQ.
     """
     args = parse_args()
-
-    if args.module:
-        import_module(args.module)
-
-    if args.flush:
-        broker = get_broker()
-        broker.flush()
-        print("Tasks flushed.")
-        sys.exit(0)
-
+    import_module(args.module)
     print("Welcome to RapidQ!")
     main_process(workers=args.workers, module_name=args.module)
     return 0
+
+
+def flush_queue():
+    broker = get_broker()
+    broker.flush()
+    print("Tasks flushed.")
+    sys.exit(0)
